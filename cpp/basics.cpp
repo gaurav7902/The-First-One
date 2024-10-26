2f03a0c57f310951ef0f66621d4df1626c94009c
Gaurav Patidar <patidargaurav4477@gmail.com>
2024-10-27 03:53:21 +0530

    Added cpp fikes

---
+++ cpp/basics.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int info (int a,int b); //prototype (needed only if function is written below the main)
+int main()
+
+
+{
+    cout << "Hello wolrd \n";
+    string str;
+    getline(cin, str); // until enter pressed
+    cout << '\n'
+         << str << '\n';
+    cin >> str; // until space 
+    cout << '\n' 
+         << str << '\n';
+         int x , y;
+         cin >>x>>y ;
+    cout << info(x,y) << '\n';
+    const int z= 5;
+    cout << z << '\n';
+    int a=7,b=5;
+    swap(a,b);
+    cout << a <<" "<<b;
+
+    return 0;
+}
+
+
+
+
+
+int info(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
